59a3885c33ad3a50808bc32cfa03c1ed452655b1
unknown <TMATOT@FR405216.corp.capgemini.com>
2012-09-17 17:35:34 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifeelfb::Application.config.secret_token = 'e09908f5dad624c88665d04a2dbbde8cc38862ad489e2cbd2d59ea259d03f2238e275578c096e3a71bcdb3bcabea08238903da2a086c182f5fbfb5b8f5f21583'
